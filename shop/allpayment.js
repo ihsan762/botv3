49a9c824d74ae37107c63fe3d288d8331078a724
ihsan76w <nurihsan762@gmail.com>
2022-01-28 11:29:40 +0800

    tes

---
+++ shop/allpayment.js
@@ -0,0 +1,36 @@
+const allpayment = (prefix) => {
+return `┏━━━━━━━━━━━━━━━━━━━━
+┃                  *PAYMENT*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━━━⊱❉ *🏧 BANK* ❉⊰━━━━✿
+┃  
+┣━⊱ *BRI*
+┣⊱ BRI ${ownername}
+┣━✿ NAMA BRI ${ownername}
+┗━━━━━━━━━━━━━━━━━━━━
+
+┏━━━━━━━━━━━━━━━━━━━━
+┃                  *PAYMENT*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━━⊱❉ *💰 E-WALLET* ❉⊰━━━✿
+┃   
+┣━⊱ *GOPAY*
+┣⊱ 0899 0000 0000
+┣━━✿ NAMA GOPAY ${ownername}
+┃
+┣━⊱ *DANA*
+┣⊱ 0899 0000 0000
+┣━━✿ NAMA DANA ${ownername}
+┃
+┣━⊱ *PULSA*
+┣⊱ 0899 0000 0000
+┣━━✿ NAMA PULSA ${ownername}
+┃
+┗━━━━━━━━━━━━━━━━━━━━
+
+NOTE! : Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
